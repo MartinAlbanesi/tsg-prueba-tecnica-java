69531f8161b33203d488dc3d8857842971c46c48
MartinAlbanesi <martinalbanesi89@gmail.com>
2025-12-12 16:06:04 -0300

    java spring boot project initialized

---
+++ src/main/java/com/martinalba/tsg_challenge/TsgChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.martinalba.tsg_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsgChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsgChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martinalba/tsg_challenge/TsgChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinalba.tsg_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsgChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
