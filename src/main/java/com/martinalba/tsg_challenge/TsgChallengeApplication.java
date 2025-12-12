08f951cb2474a02abe031fb8a0ea8b43a25cd714
MartinAlbanesi <martinalbanesi89@gmail.com>
2025-12-12 16:31:38 -0300

    created env.properties file, new User entity for db

--- src/main/java/com/martinalba/tsg_challenge/TsgChallengeApplication.java
+++ src/main/java/com/martinalba/tsg_challenge/TsgChallengeApplication.java
@@ -2,8 +2,10 @@ package com.martinalba.tsg_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class TsgChallengeApplication {
 
 	public static void main(String[] args) {
