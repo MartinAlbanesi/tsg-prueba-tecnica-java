b6266b92bbee5031056640a3228dad81702349e8
MartinAlbanesi <martinalbanesi89@gmail.com>
2025-12-17 21:12:00 -0300

    feat: new UserService to define user behavior when performing CRUD actions

---
+++ src/main/java/com/martinalba/tsg_challenge/service/UserService.java
@@ -0,0 +1,61 @@
+package com.martinalba.tsg_challenge.service;
+
+import com.martinalba.tsg_challenge.dto.request.PostUpdateRequest;
+import com.martinalba.tsg_challenge.dto.request.UserUpdateRequest;
+import com.martinalba.tsg_challenge.dto.response.PostResponse;
+import com.martinalba.tsg_challenge.dto.response.UserResponse;
+import com.martinalba.tsg_challenge.entities.Post;
+import com.martinalba.tsg_challenge.entities.User;
+import com.martinalba.tsg_challenge.mapper.PostMapper;
+import com.martinalba.tsg_challenge.mapper.UserMapper;
+import com.martinalba.tsg_challenge.repositories.IUserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<UserResponse> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::toResponse)
+                .toList();
+    }
+
+    public UserResponse getById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        return UserMapper.toResponse(user);
+    }
+
+    public UserResponse update(Long id, UserUpdateRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        if (request.getUsername() != null) {
+            user.setUsername(request.getUsername());
+        }
+
+        if (request.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+
+        userRepository.save(user);
+        return UserMapper.toResponse(user);
+    }
+
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
