570c42e550f6d620beb6c72a25da66f50fe1b4d4
MartinAlbanesi <martinalbanesi89@gmail.com>
2025-12-17 21:08:52 -0300

    fix: new posts variable in users, cascade delete for posts when the user is deleted

--- src/main/java/com/martinalba/tsg_challenge/entities/User.java
+++ src/main/java/com/martinalba/tsg_challenge/entities/User.java
@@ -3,6 +3,9 @@ package com.martinalba.tsg_challenge.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -26,5 +29,8 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
 }
 
