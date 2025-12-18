3ba62531c72c2daf4e16bad8696766a90f73e635
MartinAlbanesi <martinalbanesi89@gmail.com>
2025-12-17 21:05:49 -0300

    refactor: test endpoints excluded from authorization

--- src/main/java/com/martinalba/tsg_challenge/config/AppConfig.java
+++ src/main/java/com/martinalba/tsg_challenge/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,14 +30,15 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/**",
-                                "/api/v1/**"
+                                "/api/v1/test/welcome",
+                                "/api/v1/test/user"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
